ea4ac00195a4b6421264a33b3eb76c4574548b2b
izwrt <ishwart466@gmail.com>
2026-05-17 19:32:37 +0530

    wik: sign - up basic level explanation added

--- 05-Authentication/src/index.ts
+++ 05-Authentication/src/index.ts
@@ -6,8 +6,30 @@ const PORT = 8000
 
 app.use(express.json());
 
+type User = {
+  name: string,
+  email: string,
+  password: string
+}
+
+const dairy: Record<string, User> = {};
+const emails = new Set();
+
 // Sign up - basically collecting data from the user to authenticate in the future
 app.post("/sign-up", (req: Request, res: Response) => {
+
+  const {email, name, password} = req.body;
+
+  if(emails.has(email)){
+    return res.status(409).json({error: "Email already exists"});
+  }
+
+  const token = Date.now();
+  
+  emails.add(email);
+  dairy[token] = {name, email, password};
+
+  return res.status(201).json({message: "User has been created", token});
   
 })
 
